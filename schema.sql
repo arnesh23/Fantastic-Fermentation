fb8eacf1cfae5c7f8048a818f041990c7d3169d4
Fermenting <realferalxanderyell@gmail.com>
2019-01-09 13:51:41 -0800

    schema & seeds for greek_yogurt

--- schema.sql
+++ schema.sql
@@ -2,3 +2,28 @@
 DROP DATABASE IF EXISTS fermentor;
 -- Creates the "blogger" database --
 CREATE DATABASE fermentor;
+
+USE fermentor;
+
+CREATE TABLE greek_yogurt (
+  id INT NOT NULL AUTO_INCREMENT,
+  -- Time since the previous step in hours
+  time_interval_hrs INT(10) NOT NULL,
+
+  -- Ingredients as a string
+  ingredients VARCHAR(1000000) NOT NULL,
+
+  -- Cooking/frementing hardware as a string
+  hardware VARCHAR(1000000) NOT NULL,
+
+  -- Sub-steps of each individual step, as a string
+  sub_steps VARCHAR(1000000) NOT NULL,
+
+  --expected duration of all sub-steps in minutes
+  duration_minutes INT (1000) NOT NULL,
+
+  PRIMARY KEY (id)
+
+);
+ 
+
---
+++ seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO greek_yogurt (time_interval_hrs, ingredients, hardware, sub_steps, duration_minutes) VALUES ( 0, "1. 2 quarts good-quality Milk 2. 3-4 Tablespoons Starter yogurt (any yogurt with active live cultures, or a batch of previous home-made yogurt)", "Thermometer, good saucepan, whisk or spoon, small bowl", "1. Rinse pot with cold water to prevent scorching. 2. Stirring occaisionally, heat Milk to 180-200 degrees farenheit 3.Cool milk to 110-120 degrees 3. To small bowl add 1/2 cup warm milk and starter culture. 4. Mix well and add to pot, Mix well. 5. Cover pot  and set aside in a warm place (oven with just light turned on, or on top of fridge, or on a heating pad).", 25)
+
+INSERT INTO greek_yogurt (time_interval_hrs, ingredients, hardware, sub_steps, duration_minutes) VALUES ( 9, " ", "Bowl, Colander, Cheesecloth", "1. Line Colander with cheesecloth and place in bowl. 2. Pour yogurt into the colander and let sit in fridge to drain.", 5)
+
+INSERT INTO greek_yogurt (time_interval_hrs, ingredients, hardware, sub_steps, duration_minutes) VALUES ( 4, " " , " " , "1. Check Yogurt for thickness. 2. If it's too thick, stir some whey back in to it. 3. You're done!" , 5)
+
+-- Model for greek_yogurt
+-- INSERT INTO greek_yogurt (time_interval_hrs, ingredients, hardware, sub_steps, duration_minutes) VALUES ( ,  ,  ,  , )
